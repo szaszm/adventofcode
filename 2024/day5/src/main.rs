943887aa5d2d59052e1ba589c954a68f16920822
Marton Szasz <szaszm@apache.org>
2024-12-06 02:58:17 +0100

    day5 part2

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -1,20 +1,23 @@
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn p1_pages_follow_rules(pages: &[u32], rules: &[(u32, u32)]) -> bool {
+fn make_rule_table(rules: &[(u32, u32)]) -> HashMap<u32, Vec<(u32, u32)>> {
+    let mut rules_table: HashMap<u32, Vec<(u32, u32)>> = HashMap::new();
+    for &(pre, post) in rules {
+        rules_table.entry(pre).or_insert(Vec::new()).push((pre, post));
+        rules_table.entry(post).or_insert(Vec::new()).push((pre, post));
+    }
+    rules_table
+}
+
+fn p1_pages_follow_rules(pages: &[u32], rules: &HashMap<u32, Vec<(u32, u32)>>) -> bool {
     let mut seen: HashSet<u32> = HashSet::new();
-    let rules = {
-        let mut rules_table: HashMap<u32, Vec<u32>> = HashMap::new();
-        for &(pre, post) in rules {
-            rules_table.entry(pre).or_insert(Vec::new()).push(post);
-        }
-        rules_table
-    };
     for num in pages {
         let rules_for_num = rules.get(num);
         if let Some(rules) = rules_for_num {
-            if rules.iter().any(|post| seen.contains(post)) {
+            if rules.iter().any(|(_, post)| seen.contains(post)) {
                 return false;
             }
         }
@@ -25,7 +28,7 @@ fn p1_pages_follow_rules(pages: &[u32], rules: &[(u32, u32)]) -> bool {
 
 fn main() {
     let (rules, updates) = {
-        let file = BufReader::new(File::open("test_input.txt").expect("failed to open file"));
+        let file = BufReader::new(File::open("puzzle_input.txt").expect("failed to open file"));
         let mut rules_lines: Vec<String> = vec![];
         let mut pages_lines: Vec<String> = vec![];
         let mut rules_end = false;
@@ -53,21 +56,43 @@ fn main() {
                 .map(|num| num.parse::<u32>().expect("failed to parse number {num}"))
                 .collect::<Vec<_>>())
             .collect::<Vec<_>>();
+        let rules = make_rule_table(&rules);
         (rules, pages)
     };
     let good_updates = updates.iter().filter(|update| p1_pages_follow_rules(update, &rules));
-    let mut sum = 0;
+    let mut p1sum = 0;
     for pages in good_updates {
         let middle_number = pages.get(pages.len() / 2).unwrap();
-        sum += middle_number;
+        p1sum += middle_number;
         println!("{:?}  mid: {middle_number}", pages);
     }
-    println!("part 1: sum of middle numbers of good updates: {sum}\n");
+    println!("part 1: sum of middle numbers of good updates: {p1sum}\n");
 
     let bad_updates = updates.iter()
         .filter(|update| !p1_pages_follow_rules(update, &rules))
+        .map(|v| v.clone())
         .collect::<Vec<_>>();
-    for pages in updates {
+    let mut p2sum = 0;
+    for pages in &bad_updates {
+        let mut pages = pages.clone();
         println!("{pages:?}");
+        pages.sort_by(|a, b| {
+            rules.get(a).map(|r| {
+                for (smol, chonk) in r {
+                    if smol == b {
+                        return Ordering::Less;
+                    }
+                    if chonk == b {
+                        return Ordering::Greater;
+                    }
+                }
+                Ordering::Equal
+            }).unwrap_or(Ordering::Equal)
+        });
+        println!("after sort: {pages:?}");
+        let middle_number = pages.get(pages.len() / 2).unwrap();
+        p2sum += middle_number;
     }
+    println!("\npart 1: sum of middle numbers of good updates: {p1sum}");
+    println!("part 2: sum of middle numbers of bad updates after sort: {p2sum}");
 }
