8db2650ba984459e0396bf99a773ef2af04ef1b0
Marton Szasz <szaszm@apache.org>
2024-12-08 23:21:47 +0100

    day7 part2

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -6,7 +6,7 @@ struct Equation {
     numbers: Vec<u64>
 }
 
-fn run_numbers(numbers: &[u64], operations_bitset: u64) -> u64 {
+fn run_numbers_part1(numbers: &[u64], operations_bitset: u64) -> u64 {
     let mut acc = *numbers.first().expect("first number");
     for i in 1..numbers.len() {
         let bit = operations_bitset & ((1 << (numbers.len() - 1)) >> i) != 0;
@@ -19,7 +19,30 @@ fn run_numbers(numbers: &[u64], operations_bitset: u64) -> u64 {
     acc
 }
 
-fn print_solution(numbers: &[u64], operations_bitset: u64) {
+fn run_numbers_part2(numbers: &[u64], operations_base3: u64) -> u64 {
+    let mut acc = *numbers.first().expect("first number");
+    //print!("{}", acc);
+    let mut op_acc = operations_base3;
+    for i in 1..numbers.len() {
+        let opcode = op_acc % 3;
+        op_acc /= 3;
+        if opcode == 0 {
+            //print!(" * {}", numbers[i]);
+            acc *= numbers[i];
+        } else if opcode == 1 {
+            //print!(" + {}", numbers[i]);
+            acc += numbers[i];
+        } else if opcode == 2 {
+            //print!(" || {}", numbers[i]);
+            let digits = numbers[i].ilog10() + 1;
+            acc *= 10_u64.pow(digits);
+            acc += numbers[i];
+        } else { panic!("invalid opcode"); }
+    }
+    //println!(" = {}", acc);
+    acc
+}
+fn print_solution_part1(numbers: &[u64], operations_bitset: u64) {
     let mut acc = *numbers.first().expect("first number");
     print!("{}", acc);
     for i in 1..numbers.len() {
@@ -48,17 +71,36 @@ fn main() {
         equations.push(Equation{ test_value, numbers });
     }
 
+    // part1
     let mut sum = 0;
-    for eq in equations {
+    for eq in &equations {
         let len = eq.numbers.len();
+        //let mut found = false;
         for i in 0..(1 << (len - 1)) {
-            let n = run_numbers(&eq.numbers, i);
+            let n = run_numbers_part1(&eq.numbers, i);
             if n == eq.test_value {
-                print_solution(&eq.numbers, i);
+                //print_solution_part1(&eq.numbers, i);
+                sum += eq.test_value;
+                //found = true;
+                break;
+            }
+        }
+        //if !found { println!("no solution found for line:  {}: {:?}", eq.test_value, eq.numbers); }
+    }
+    println!("part1 sum: {sum}");
+
+    // part2
+    let mut sum = 0;
+    for eq in &equations {
+        let len = eq.numbers.len() as u32;
+        let limit = 3_u64.pow(len - 1);
+        for i in 0..limit {
+            let n = run_numbers_part2(&eq.numbers, i);
+            if n == eq.test_value {
                 sum += eq.test_value;
                 break;
             }
         }
     }
-    println!("part1 sum: {sum}");
+    println!("part2 sum: {sum}");
 }
